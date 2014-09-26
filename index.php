d3eadd6e5cfe65108be7d7764e2b39f6e2ce3014
gringogidget <kristinagroeger@gmail.com>
2014-09-25 21:25:08 -0400

    f-ing around w php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  //this is a comment
+  
+  /*This is 
+  a multiple
+  Line comment
+
+Why is this not saving?
+
+  */
+
+  $mycounter = 1;
+  $mystring  = "Hello";
+  $myarray   = array("One", "Two", "Three"); 
+
+?>
